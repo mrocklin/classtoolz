4c04e5c8448f4ebb4b549b18417a6f660354180f
Matthew Rocklin <mrocklin@gmail.com>
2013-07-12 17:45:50 -0500

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from os.path import exists
 from setuptools import setup
 
 setup(name='classtoolz',
-      version='0.1',
+      version='0.1.1',
       description='Class utilities',
       url='http://github.com/mrocklin/classtoolz',
       author='Matthew Rocklin',
